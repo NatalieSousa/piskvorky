40e6f3cc44ed595f7ea59b44473c52cf2e1a3aaa
Natalie Ferreira <natalie.fsousa@gmail.com>
2020-10-25 19:17:08 +0100

    prvni verze javascript

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//
+//-	Označit, že vždycky začíná kolečko
+//-	Říct, že kolečko a čtverečky budou na buttonech
+//-	Když hraje kolečko, tak se vypíše obrázek kolečka do butonu, ale zároveň i nahoře
+//-	Když hraje křížek, tak sevypíše křížek do butonnu, ale zároveň i nahoře
+
+//  když na tahu kolečko, tak v buttonu kolečko, takže informace, že potom hraje křížek, změní se proměnná na tahu ze circle na křížek
+
+console.log('Funguju!');
+
+let naTahu = 'circle';
+
+let hraje = document.querySelector("symbol");
+
+const pole = document.querySelectorAll("btn");
+
+const hra = (event) => {
+  if ( naTahu === 'circle') {
+    event.target.classList.add("board__field--circle");
+naTahu = "cross";
+hraje.classList.add("board__field--cross");
+  }
+  else {
+    naTahu === "cross"
+  }
+};
+
+//cyklus - který mi zavolá funkci hra
+
+for (let i = 0; i < pole.length; i++) {
+   pole[i].addEventListener("click", hra); 
+}
